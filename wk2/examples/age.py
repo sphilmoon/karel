34c9ec17da99861dab1e3c7ffce5ba4b5c5316e0
sphilmoon <sphilmoon@gmail.com>
2021-06-01 19:30:40 +0900

    defining the age with constant values.

---
+++ wk2/examples/age.py
@@ -0,0 +1,15 @@
+ANTON = 21
+BETH = ANTON + 6
+CHEN = BETH + 20
+DREW = CHEN + ANTON
+ETHAN = CHEN
+
+def main():
+	print("Anton is", ANTON, "years old.")
+	print("Beth is", BETH, "years old.")
+	print("Chen is", CHEN, "years old.")
+	print("Drew is", DREW, "years old.")
+	print("Ethan is", ETHAN, "years old.")
+
+if __name__ == '__main__':
+    main()
