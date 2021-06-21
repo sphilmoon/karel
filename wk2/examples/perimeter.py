6cfd36834a6da1c64788fe6b89ab685fc80397de
sphilmoon <sphilmoon@gmail.com>
2021-06-21 20:00:13 +0900

    determining a triangle perimeter.

---
+++ wk2/examples/perimeter.py
@@ -0,0 +1,8 @@
+def main():
+    side1 = float(input("What is the length of side 1? "))
+    side2 = float(input("What is the length of side 2? "))
+    side3 = float(input("What is the length of side 3? "))
+    print("The perimeter of the triangle is", side1+side2+side3)
+
+if __name__ == "__main__":
+    main()
