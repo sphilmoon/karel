ac8bdd5cd1d7fbaceae89b39c56a9ed9e817bf43
sphilmoon <sphilmoon@gmail.com>
2021-09-14 16:06:00 +0900

    practising functions and arguments.

---
+++ wk4_images/return_exercise.py
@@ -0,0 +1,11 @@
+def main():
+    result = multiply(2, 5)
+
+    # print(result)
+
+def multiply(first, second):
+    multi_result = first * second
+    return multi_result
+
+if __name__ == "__main__":
+    main()
